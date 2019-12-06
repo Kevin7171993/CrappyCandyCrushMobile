23ac26f662e368d17fe19aae23a0bea4ded1f582
LCVClass <Kevin7171993@hotmail.com>
2019-12-06 11:12:01 -0800

    Initial Commit

---
+++ CrappyCrush/CrappyCrush/Assets/Scripts/Board.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public Vector2 boardSize; // 5x5
+    public Vector2 cellSize; //1x1
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector3(boardSize.x, boardSize.y);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ CrappyCrush/CrappyCrush/Assets/Scripts/Shape.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Shapes
+{
+    star,
+    circle,
+    square,
+    triangle,
+    Pentagon,
+    count
+}
+
+public class Shape : MonoBehaviour
+{
+    public Shapes mShape;
+    public Vector2 gridPos;
+    public bool Selected = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+}

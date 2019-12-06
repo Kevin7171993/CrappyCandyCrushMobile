f4807d83887dfa3c290ac8d15bcb8c404cc04580
LCVClass <Kevin7171993@hotmail.com>
2019-12-06 12:45:13 -0800

    Changes to be committed: 	new file:   CrappyCrush/CrappyCrush/Assets/Prefab.meta 	new file:   CrappyCrush/CrappyCrush/Assets/Prefab/Shape.prefab 	new file:   CrappyCrush/CrappyCrush/Assets/Prefab/Shape.prefab.meta 	modified:   CrappyCrush/CrappyCrush/Assets/Scenes/SampleScene.unity 	modified:   CrappyCrush/CrappyCrush/Assets/Scripts/Board.cs 	modified:   CrappyCrush/CrappyCrush/Assets/Scripts/Shape.cs 	new file:   CrappyCrush/CrappyCrush/Assets/Sprites/Images/Materials.meta 	new file:   CrappyCrush/CrappyCrush/Assets/Sprites/Images/Materials/Circle.mat 	new file:   CrappyCrush/CrappyCrush/Assets/Sprites/Images/Materials/Circle.mat.meta 	modified:   CrappyCrush/CrappyCrush/Library/CurrentLayout.dwlt 	modified:   CrappyCrush/CrappyCrush/Library/InspectorExpandedItems.asset 	modified:   CrappyCrush/CrappyCrush/Library/ScriptAssemblies/Assembly-CSharp.dll 	modified:   CrappyCrush/CrappyCrush/Library/StateCache/SceneView/2a4edb-mainStage.json 	new file:   CrappyCrush/CrappyCrush/Library/UIElements/EditorWindows/UnityEditor.InspectorWindow.pref 	modified:   CrappyCrush/CrappyCrush/Library/assetDatabase3 	modified:   CrappyCrush/CrappyCrush/Library/metadata/04/0491b7f74f4ad384e87c05c73e7d7776 	new file:   CrappyCrush/CrappyCrush/Library/metadata/2b/2bdd9759a6a7b2a42a30eeba503e9706 	new file:   CrappyCrush/CrappyCrush/Library/metadata/2b/2bdd9759a6a7b2a42a30eeba503e9706.info 	new file:   CrappyCrush/CrappyCrush/Library/metadata/4d/4dd5c237ff930694685d96936396a49d 	new file:   CrappyCrush/CrappyCrush/Library/metadata/4d/4dd5c237ff930694685d96936396a49d.info 	new file:   CrappyCrush/CrappyCrush/Library/metadata/a6/a6a7c322156a37f439d419be3d9350a7 	new file:   CrappyCrush/CrappyCrush/Library/metadata/a6/a6a7c322156a37f439d419be3d9350a7.info 	modified:   CrappyCrush/CrappyCrush/Library/metadata/a9/a9caa29d5c7936d42ab82e729f38ef7c 	new file:   CrappyCrush/CrappyCrush/Library/metadata/b9/b96efee36079c9f4a9df38b74059ccea 	new file:   CrappyCrush/CrappyCrush/Library/metadata/b9/b96efee36079c9f4a9df38b74059ccea.info 	new file:   CrappyCrush/CrappyCrush/obj/Debug/Assembly-CSharp.csproj.CopyComplete 	modified:   CrappyCrush/CrappyCrush/obj/Debug/Assembly-CSharp.csproj.CoreCompileInputs.cache 	new file:   CrappyCrush/CrappyCrush/obj/Debug/Assembly-CSharp.csproj.FileListAbsolute.txt 	modified:   CrappyCrush/CrappyCrush/obj/Debug/Assembly-CSharp.csprojAssemblyReference.cache 	new file:   CrappyCrush/CrappyCrush/obj/Debug/Assembly-CSharp.dll

--- CrappyCrush/CrappyCrush/Assets/Scripts/Board.cs
+++ CrappyCrush/CrappyCrush/Assets/Scripts/Board.cs
@@ -4,13 +4,85 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    public GameObject shapeObj;
     public Vector2 boardSize; // 5x5
     public Vector2 cellSize; //1x1
+    public Vector2 firstCell;
+    public GameObject[] shapeList;
+
+    public Vector2 TopLeft()
+    {
+        int x, y;
+        x = (int)(-(boardSize.x / 2));
+        y = (int)(boardSize.y / 2);
+        return new Vector2(x, y);
+    }
+    public Vector2 BottomRight()
+    {
+        int x, y;
+        x = (int)-(TopLeft().x);
+        y = (int)-(TopLeft().y);
+        return new Vector2(x, y);
+    }
+    public void Populate()
+    {
+        int sx = (int)TopLeft().x;
+        int sy = (int)TopLeft().y;
+        int ix = 0;
+        int iy = 0;
+        for (int i = 0; i < shapeList.Length; i++)
+        {
+            if(shapeList[i] == null)
+            {
+                shapeList[i] = Instantiate(shapeObj);
+                shapeList[i].GetComponent<Shape>().SetShapePos(sx + ix, sy + iy);
+                shapeList[i].GetComponent<Shape>().UpdatePos();
+                if (ix >= (int)boardSize.x - 1)
+                {
+                    ix = 0;
+                    iy--;
+                }
+                else
+                {
+                    ix++;
+                }
+                if(iy <= BottomRight().y)
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    public void AddShape(Vector2 pos)
+    {
+        
+
+    }
+    /*
+     x0123456  7x5
+     0#######
+     1#######
+     2###v###
+     3#######
+     4#######
+     */ 
+
+    public int GetGridIndex(Vector2 pos)
+    {
+        int i = (int)(pos.x * pos.y) + (int)pos.x; //7 * 2 + 3 = 17 (width * pos.y + pos.x)
+        return i;
+    }
     // Start is called before the first frame update
     void Start()
     {
         transform.localScale = new Vector3(boardSize.x, boardSize.y);
-
+        firstCell = TopLeft();
+        shapeList = new GameObject[(int)(boardSize.x * boardSize.y)]; //Create an array of shapes based on board size
+        //shapeList[0] = Instantiate(shapeObj);
+        //shapeList[0].GetComponent<Shape>().SetShapePos((int)TopLeft().x, (int)TopLeft().y);
+        //shapeList[0].GetComponent<Shape>().UpdatePos();
+        Populate();
     }
 
     // Update is called once per frame
--- CrappyCrush/CrappyCrush/Assets/Scripts/Shape.cs
+++ CrappyCrush/CrappyCrush/Assets/Scripts/Shape.cs
@@ -17,10 +17,28 @@ public class Shape : MonoBehaviour
     public Shapes mShape;
     public Vector2 gridPos;
     public bool Selected = false;
+    public Sprite currentSprite;
+    public Sprite[] listSprite = new Sprite[(int)Shapes.count];
+    private SpriteRenderer sr;
+
+
+    public void SetShapePos(int x, int y)
+    {
+        gridPos.x = x;
+        gridPos.y = y;
+    }
+    public void UpdatePos()
+    {
+        transform.position = gridPos;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        int r = Random.Range(0, (int)Shapes.count);
+        currentSprite = listSprite[r];
+        sr.sprite = currentSprite;
+        mShape = (Shapes)r;
     }
 
     // Update is called once per frame
